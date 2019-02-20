4095217557fbb3aa5ea0ee2cd7feeb594b04085c
Goran.Mitkovski <Goran.Mitkovski@TERMALIFT.LOKAL>
2019-02-20 13:15:08 +0100

    Domashna2 zakachena bez node_moduls

---
+++ Vezba 2/index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+ 
+
+
+
+
+ fs.readFile("users.json", "utf-8", (err, data) => {
+    if(err) {
+        throw err;
+    } else {
+       var parsedJSON = JSON.parse(data);
+       var iminja ="";
+
+       for(let i = 0 ; i < parsedJSON.length; i++) {
+           if(parsedJSON[i]) {
+
+               iminja = iminja + " " + parsedJSON[i].name;
+               
+           }
+       }
+
+       http.createServer((request, response) => {
+        
+        if(request.url === "/home") {
+           
+            response.writeHead(200,"OK");
+            response.write(iminja);
+            response.end();
+        } else {
+            response.writeHead(400,"OK");
+            response.write("Error");
+            response.end();
+        }
+    
+    }).listen(3000);
+
+      
+    }
+});
+
+
+
+
+     
+// // Kreirame Server 
+ 
