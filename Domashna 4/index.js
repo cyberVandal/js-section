f9056d0b521641807d1da4fdc53b348928a82243
Goran.Mitkovski <Goran.Mitkovski@TERMALIFT.LOKAL>
2019-03-21 13:37:02 +0100

    Domashna 4

--- Domashna 4/index.js
+++ Domashna 4/index.js
@@ -5,5 +5,14 @@ const fetch = require('node-fetch');
 const port = 3000;
 
 
-app.set('view-engine','ejs');
+app.set('view engine','ejs');
 app.listen(port, ()=>{console.log("RABOTI NA: "+port)});
+
+
+app.get('/', (req, res) => {
+    fetch('https://dog.ceo/api/breed/retriever/golden/images')
+    .then(res => res.json())
+    .then(json => {
+        res.render('index', { dogs: json})
+    })
+})
